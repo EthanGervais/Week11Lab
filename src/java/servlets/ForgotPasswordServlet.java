a0055f94dca9f36572041734499addb2b8318430
EthanGervais <EthanGervais@users.noreply.github.com>
2020-11-30 00:25:35 -0500

    Project Re-upload Re-uploading for password reasons lol.

---
+++ src/java/servlets/ForgotPasswordServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import services.AccountService;
+
+/**
+ *
+ * @author Ethan Gervais
+ */
+public class ForgotPasswordServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        AccountService as = new AccountService();
+        String email = request.getParameter("email");
+        String path = getServletContext().getRealPath("/WEB-INF");
+        boolean forgotPassword = as.forgotPassword(email, path);
+
+        String message = "If the address you entered is valid, you will receive an email very soon. Please check your email for your password.";
+
+        if (forgotPassword == true) {
+            request.setAttribute("message", message);
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/forgot.jsp").forward(request, response);
+    }
+
+}
